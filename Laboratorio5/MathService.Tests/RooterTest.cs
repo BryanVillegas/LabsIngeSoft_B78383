a48bd25127ee9ae47625e66178369d89960b343c
BryanVillegas <bryan.villegasalvarado@ucr.ac.cr>
2023-06-11 11:51:04 -0600

    Lab5 added

---
+++ Laboratorio5/MathService.Tests/RooterTest.cs
@@ -0,0 +1,48 @@
+﻿using MathService;
+using Xunit;
+
+namespace MathService.Tests
+{
+    public class RooterTest
+    {
+        [Fact]
+        public void BasicRooterTest()
+        {
+            Rooter rooter = new Rooter();
+
+            double expectedResult = 2.0;
+            double input = expectedResult * expectedResult;
+
+            double actualResult = rooter.SquareRoot(input);
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void RooterValueRange()
+        {
+            Rooter rooter = new Rooter();
+
+            for (double expected = 1e-8; expected < 1e+8; expected *=3.2)
+            {
+                RooterOneValue(rooter, expected);
+            }
+        }
+
+        private void RooterOneValue(Rooter rooter, double expectedResult)
+        {
+            double input = expectedResult * expectedResult;
+            double actualResult = rooter.SquareRoot(input);
+            //Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 1000);
+            Assert.InRange(actualResult, expectedResult - expectedResult / 1000, expectedResult + expectedResult / 1000);
+        }
+
+
+        [Fact]
+        public void RooterTestNegativeInput()
+        {
+            Rooter rooter = new Rooter();
+            Assert.Throws<ArgumentOutOfRangeException>(() => rooter.SquareRoot(-1));
+        }
+    }
+}
